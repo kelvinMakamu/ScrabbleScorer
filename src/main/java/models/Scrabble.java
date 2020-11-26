4ba18faff69f331b8d128306a592a3970152fa67
Kelvin Makamu <kmakamu@safaricom.co.ke>
2020-11-26 22:03:10 +0300

    Modified Scrabble class to include a scoring matrix method

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -5,6 +5,9 @@
  */
 package models;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  *
  * @author KMAKAMU
@@ -12,7 +15,29 @@ package models;
 public class Scrabble {
     
     public Integer calculateScore(String word){
-       Integer  result = 1;
+        char[] letters = word.toUpperCase().toCharArray();
+        Map<Character,Integer> mapScores = scoringMapping();
+        Integer result = 0;
+        for(int i=0; i<letters.length;i++){
+            if(mapScores.containsKey(letters[i])){
+                result += mapScores.get(letters[i]);
+            }
+        }
         return result;
+    }
+    
+    public Map<Character,Integer> scoringMapping (){
+        Map<Character,Integer> mapScores = new LinkedHashMap<>();
+        mapScores.put('A',1);
+        mapScores.put('E',1);
+        mapScores.put('I',1);
+        mapScores.put('O',1);
+        mapScores.put('U',1);
+        mapScores.put('L',1);
+        mapScores.put('N',1);
+        mapScores.put('R',1);
+        mapScores.put('S',1);
+        mapScores.put('T',1);
+        return mapScores;
     }
 }
