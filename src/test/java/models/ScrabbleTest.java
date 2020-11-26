9d0543b40f40b39218450021370f260277d30405
Kelvin Makamu <kmakamu@safaricom.co.ke>
2020-11-26 21:21:47 +0300

    Adds first JUnit Test for a single-character word

---
+++ src/test/java/models/ScrabbleTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author KMAKAMU
+ */
+public class ScrabbleTest {
+    
+    public ScrabbleTest() {
+    }
+    
+    @Test
+    public void calculateScore_returnScoreForASingleLetter_Integer(){
+        Scrabble testScrabble = new Scrabble();
+        Integer expected = 1;
+        assertEquals(expected, testScrabble.calculateScore("a"));
+    }
+    
+    
+    
+}
