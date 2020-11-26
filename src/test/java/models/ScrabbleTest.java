4c22c4868bd1b215e369bb519f00b4779cd0d92c
Kelvin Makamu <kmakamu@safaricom.co.ke>
2020-11-26 22:36:08 +0300

    Added test case method to calculate score for any letter

--- src/test/java/models/ScrabbleTest.java
+++ src/test/java/models/ScrabbleTest.java
@@ -5,6 +5,8 @@
  */
 package models;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -29,6 +31,46 @@ public class ScrabbleTest {
         Scrabble testScrabble = new Scrabble();
         Integer expected  = 2;
         assertEquals(expected, testScrabble.calculateScore("ae"));
+    }
+    
+    @Test
+    public void calculateScore_retturnScoreForASingleLetterFromAnyCategory_Integer(){
+        Scrabble testScrabble = new Scrabble();
+        String word = "z";
+        char [] letters = word.toUpperCase().toCharArray();
+        char character = letters[0];
+        Map<Character,Integer> mapScores = new LinkedHashMap<>();
+        mapScores.put('A',1);
+        mapScores.put('E',1);
+        mapScores.put('I',1);
+        mapScores.put('O',1);
+        mapScores.put('U',1);
+        mapScores.put('L',1);
+        mapScores.put('N',1);
+        mapScores.put('R',1);
+        mapScores.put('S',1);
+        mapScores.put('T',1);
+        mapScores.put('D',2);
+        mapScores.put('G',2);
+        mapScores.put('B',3);
+        mapScores.put('C',3);
+        mapScores.put('M',3);
+        mapScores.put('P',3);
+        mapScores.put('F',4);
+        mapScores.put('H',4);
+        mapScores.put('V',4);
+        mapScores.put('W',4);
+        mapScores.put('Y',4);
+        mapScores.put('K',5);
+        mapScores.put('J',8);
+        mapScores.put('X',8);
+        mapScores.put('Q',10);
+        mapScores.put('Z',10);
+        Integer expected = 0;
+        if(mapScores.containsKey(character)){
+            expected +=mapScores.get(character);
+        }
+        assertEquals(expected,testScrabble.calculateScore(word));
     }
     
     
